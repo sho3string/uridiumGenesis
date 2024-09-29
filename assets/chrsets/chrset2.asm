884b99608fb43fd67548c9da6653273fce18c7a5
Samuel <36328867+sho3string@users.noreply.github.com>
2024-09-29 14:09:56 +1000

    Added scroll text tiles

---
+++ assets/chrsets/chrset2.asm
@@ -0,0 +1,258 @@
+; Uridium - scroll text character set
+UridiumCharSet2:
+	; Tile index,$000
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$00,$11,$00,$00,$00,$cc,$11,$11,$00,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc
+	; Tile index,$001
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$00,$11,$11,$11,$00,$11,$11,$11,$00,$11,$11,$11,$00,$11,$11,$11,$00,$11,$11,$11,$00
+	; Tile index,$002
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$00,$11,$00,$00,$00,$11,$00,$cc,$11,$11,$00,$11,$11,$11,$00,$11,$11,$11,$00,$11,$00
+	; Tile index,$003
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$11,$ff,$00,$00,$cc,$cc,$11,$00,$cc,$cc,$11,$ff,$cc,$ff,$11,$00,$cc,$11,$00,$00,$cc,$ff
+	; Tile index,$004
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$cc,$ff,$00,$00,$ff,$ff,$00,$00,$ff,$11,$11,$11,$cc,$11,$11,$11,$11,$11,$11,$11,$cc,$ff,$cc,$11
+	; Tile index,$005
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$cc,$11,$00,$00,$00,$ff,$11,$ff,$00,$ff,$11,$11,$00,$11,$11,$11,$00,$11,$11,$11,$00,$11
+	; Tile index,$006
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$00,$11,$00,$11,$11,$cc,$00,$11,$11,$cc,$00,$11,$00,$11,$00,$11,$11,$11,$00,$11,$00,$11,$00,$11
+	; Tile index,$007
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc
+	; Tile index,$008
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$11,$ff,$00,$00,$cc,$ff,$00,$ff,$00,$ff,$cc,$11,$00,$ff,$cc,$11,$ff,$ff,$cc,$11,$11
+	; Tile index,$009
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$11,$ff,$00,$00,$cc,$00,$cc,$00,$cc,$00,$11,$ff,$cc,$cc,$11,$ff,$cc,$11,$11,$ff,$cc
+	; Tile index,$00A
+	dc.b $11,$11,$ff,$00,$11,$11,$ff,$00,$11,$11,$11,$00,$11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$00,$00,$00,$00,$00,$00,$11,$11,$11,$11
+	; Tile index,$00B
+	dc.b $11,$11,$ff,$00,$00,$00,$00,$00,$00,$00,$00,$cc,$11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$00,$00,$00,$00,$00,$00,$11,$11,$11,$11
+	; Tile index,$00C
+	dc.b $11,$00,$11,$00,$11,$00,$11,$ff,$11,$00,$11,$11,$11,$00,$11,$11,$ff,$00,$11,$11,$00,$00,$11,$11,$00,$cc,$11,$11,$11,$11,$11,$11
+	; Tile index,$00D
+	dc.b $00,$00,$11,$ff,$00,$cc,$11,$ff,$00,$00,$11,$ff,$ff,$00,$cc,$ff,$11,$00,$00,$ff,$11,$ff,$00,$00,$11,$11,$00,$00,$11,$11,$11,$11
+	; Tile index,$00E
+	dc.b $cc,$ff,$00,$11,$cc,$ff,$00,$00,$cc,$11,$00,$00,$cc,$11,$11,$11,$cc,$11,$11,$11,$00,$00,$00,$00,$00,$00,$00,$00,$11,$11,$11,$11
+	; Tile index,$00F
+	dc.b $11,$ff,$00,$11,$00,$00,$00,$11,$00,$00,$cc,$11,$11,$11,$11,$11,$11,$11,$11,$ff,$00,$00,$00,$00,$00,$00,$00,$00,$11,$11,$11,$11
+	; Tile index,$010
+	dc.b $00,$11,$00,$cc,$00,$11,$00,$00,$00,$11,$ff,$00,$00,$11,$11,$11,$00,$11,$11,$11,$00,$11,$11,$00,$cc,$11,$11,$00,$11,$11,$11,$11
+	; Tile index,$011
+	dc.b $11,$11,$00,$cc,$00,$00,$00,$cc,$00,$00,$00,$11,$11,$11,$11,$11,$11,$11,$11,$11,$00,$00,$00,$00,$00,$00,$00,$00,$11,$11,$11,$11
+	; Tile index,$012
+	dc.b $ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$00,$cc,$11,$11,$00,$cc,$11,$11,$00,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$013
+	dc.b $11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$ff,$cc,$11,$11,$11,$11
+	; Tile index,$014
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$015
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$016
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$017
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$018
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$019
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$01A
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$01B
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$01C
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$01D
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$01E
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$01F
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$020 - black tile.
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$021
+	dc.b $11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$cc,$cc,$cc,$cc,$cc,$cc,$cc
+	; Tile index,$022
+	dc.b $11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff
+	; Tile index,$023
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff
+	; Tile index,$024
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc
+	; Tile index,$025
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$026
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$027
+	dc.b $ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc
+	; Tile index,$028
+	dc.b $ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc
+	; Tile index,$029
+	dc.b $11,$11,$11,$11,$11,$11,$11,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc
+	; Tile index,$02A
+	dc.b $11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$02B
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$11,$00,$00,$11,$11,$00,$11,$11,$ff,$11,$11,$ff,$ff
+	; Tile index,$02C
+	dc.b $00,$00,$00,$11,$00,$00,$11,$11,$00,$11,$11,$ff,$11,$11,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$02D
+	dc.b $ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$02E
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$11,$00,$00,$00,$ff,$11,$00,$00,$ff,$ff,$11,$00
+	; Tile index,$02F
+	dc.b $00,$00,$00,$00,$11,$00,$00,$00,$ff,$11,$00,$00,$ff,$ff,$11,$00,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$030
+	dc.b $00,$cc,$ff,$ff,$00,$00,$cc,$ff,$00,$00,$00,$cc,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$031
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff
+	; Tile index,$032
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$00,$cc,$ff,$ff,$00,$00,$cc,$ff,$00,$00,$00,$cc,$00,$00,$00,$00
+	; Tile index,$033
+	dc.b $ff,$ff,$cc,$cc,$ff,$cc,$cc,$00,$cc,$cc,$00,$00,$cc,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$034
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc
+	; Tile index,$035
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$cc,$cc,$ff,$cc,$cc,$00,$cc,$cc,$00,$00,$cc,$00,$00,$00
+	; Tile index,$036
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$ff,$ff,$11,$11,$ff,$11,$11,$ff,$11,$11,$ff,$ff
+	; Tile index,$037
+	dc.b $ff,$ff,$ff,$11,$ff,$ff,$11,$11,$ff,$11,$11,$ff,$11,$11,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$038
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$11,$ff
+	; Tile index,$039
+	dc.b $ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$03A
+	dc.b $ff,$cc,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$03B
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff
+	; Tile index,$03C
+	dc.b $ff,$ff,$cc,$cc,$ff,$cc,$cc,$ff,$cc,$cc,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$03D
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$cc,$cc,$ff,$cc,$cc,$ff,$cc,$cc,$ff,$ff,$cc,$ff,$ff,$ff
+	; Tile index,$03E
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$11,$ff,$ff,$ff
+	; Tile index,$03F
+	dc.b $11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$040
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc
+	; Tile index,$041
+	dc.b $ff,$ff,$ff,$cc,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$042
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$11,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$043
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$11,$00,$00,$00,$11,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$044
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$cc,$cc,$cc,$11,$cc,$cc,$11,$11,$cc,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$045
+	dc.b $cc,$cc,$00,$00,$cc,$cc,$00,$00,$cc,$cc,$00,$00,$cc,$cc,$00,$00,$cc,$cc,$00,$00,$cc,$cc,$00,$00,$cc,$cc,$00,$00,$cc,$cc,$00,$00
+	; Tile index,$046
+	dc.b $00,$00,$cc,$cc,$00,$00,$11,$cc,$00,$00,$11,$11,$00,$00,$11,$11,$00,$00,$11,$11,$00,$00,$11,$11,$00,$00,$11,$11,$00,$00,$11,$11
+	; Tile index,$047
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$048
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$cc,$11,$cc,$cc,$11,$11,$cc,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$049
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff
+	; Tile index,$04A
+	dc.b $ff,$cc,$cc,$cc,$ff,$ff,$cc,$11,$ff,$ff,$11,$11,$ff,$ff,$11,$11,$ff,$ff,$11,$11,$ff,$ff,$11,$11,$ff,$ff,$11,$11,$ff,$ff,$11,$11
+	; Tile index,$04B
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$ff,$ff
+	; Tile index,$04C
+	dc.b $ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff
+	; Tile index,$04D
+	dc.b $cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff
+	; Tile index,$04E
+	dc.b $ff,$cc,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$04F
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$050
+	dc.b $cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$051
+	dc.b $cc,$cc,$11,$11,$cc,$cc,$cc,$11,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff
+	; Tile index,$052
+	dc.b $cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff
+	; Tile index,$053
+	dc.b $cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc
+	; Tile index,$054
+	dc.b $ff,$ff,$ff,$ff,$11,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$055
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$11,$11,$ff,$11,$11,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$056
+	dc.b $ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$11,$ff,$ff,$11,$11,$11,$ff,$11,$11,$11,$ff,$11,$11,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$057
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$11,$ff,$ff,$11,$11,$ff,$11,$11,$11,$ff,$11,$11,$11,$ff,$ff,$11,$11,$ff,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$058
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$ff,$11,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$059
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$11,$cc,$00,$00,$11,$cc,$00,$00,$11,$cc,$00,$00,$11,$cc,$00,$00,$11,$cc,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$05A
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$05B
+	dc.b $00,$00,$00,$00,$00,$00,$00,$00,$ff,$11,$11,$11,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$cc,$cc,$cc,$00,$00,$00,$00,$00,$00,$00,$00
+	; Tile index,$05C
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc
+	; Tile index,$05D
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$11,$cc,$cc,$11,$11,$cc,$11,$11,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$05E
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$05F
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc
+	; Tile index,$060
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$cc,$cc,$11,$cc,$cc,$11,$11,$cc,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$061
+	dc.b $11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11,$11
+	; Tile index,$062
+	dc.b $cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff,$cc,$cc,$ff,$ff
+	; Tile index,$063
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$11,$ff,$11,$cc,$cc,$11,$cc,$cc,$ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$ff
+	; Tile index,$064
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$11,$11,$11,$cc,$cc,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$cc,$11,$ff,$cc,$11,$cc
+	; Tile index,$065
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$11,$ff,$ff,$cc,$cc,$cc,$ff,$cc,$11,$cc,$cc,$11,$cc,$11,$cc,$cc,$ff,$11,$cc,$ff,$ff,$11,$cc
+	; Tile index,$066
+	dc.b $11,$cc,$ff,$ff,$11,$cc,$ff,$cc,$11,$cc,$cc,$11,$11,$cc,$11,$cc,$ff,$cc,$cc,$11,$ff,$ff,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$067
+	dc.b $cc,$11,$cc,$ff,$11,$cc,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$11,$11,$11,$cc,$cc,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$068
+	dc.b $ff,$ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$11,$11,$cc,$11,$11,$cc,$ff,$cc,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$069
+	dc.b $ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$ff,$cc,$11,$ff,$ff,$cc,$11,$ff,$ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$ff,$cc,$11,$ff,$ff,$cc,$11,$ff
+	; Tile index,$06A
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$cc,$11,$cc,$11,$cc,$11,$cc,$cc,$11,$cc,$11,$cc,$11,$cc,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$06B
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$11,$cc,$ff,$cc,$11,$cc,$ff,$11,$cc,$11,$ff,$11,$cc,$11,$ff,$cc,$11,$ff,$ff,$cc,$11,$ff
+	; Tile index,$06C
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$11,$cc,$11,$ff,$11,$cc,$11,$ff,$cc,$11,$cc,$ff,$cc,$11,$cc,$ff,$ff,$cc,$11,$ff,$ff,$cc,$11,$ff
+	; Tile index,$06D
+	dc.b $ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$ff,$cc,$11,$cc,$ff,$cc,$11,$cc,$ff,$11,$cc,$11,$ff,$11,$cc,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$06E
+	dc.b $ff,$11,$cc,$ff,$ff,$11,$cc,$ff,$11,$cc,$11,$ff,$11,$cc,$11,$ff,$cc,$11,$cc,$ff,$cc,$11,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$06F
+	dc.b $ff,$ff,$ff,$ff,$11,$ff,$ff,$ff,$11,$11,$ff,$ff,$11,$11,$11,$ff,$11,$11,$11,$ff,$11,$11,$ff,$ff,$11,$ff,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$070
+	dc.b $ff,$ff,$11,$ff,$ff,$ff,$cc,$11,$ff,$ff,$ff,$cc,$ff,$11,$00,$cc,$ff,$cc,$11,$ff,$00,$ff,$cc,$cc,$cc,$ff,$00,$cc,$cc,$11,$cc,$cc
+	; Tile index,$071
+	dc.b $ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$11,$00,$11,$ff,$cc,$00,$cc,$ff,$cc,$ff,$ff,$ff,$cc,$00,$cc,$ff,$cc,$cc,$00,$11,$ff,$cc,$ff,$cc
+	; Tile index,$072
+	dc.b $ff,$cc,$00,$ff,$ff,$cc,$ff,$cc,$00,$cc,$cc,$ff,$11,$11,$cc,$cc,$cc,$cc,$ff,$cc,$ff,$00,$11,$cc,$11,$cc,$cc,$ff,$ff,$cc,$ff,$ff
+	; Tile index,$073
+	dc.b $cc,$cc,$ff,$ff,$11,$00,$11,$ff,$cc,$11,$cc,$ff,$cc,$cc,$11,$cc,$00,$ff,$cc,$ff,$cc,$ff,$00,$ff,$00,$cc,$ff,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$074
+	dc.b $ff,$ff,$cc,$ff,$11,$ff,$11,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$cc,$cc,$ff,$cc,$ff,$cc,$cc,$00,$ff,$ff,$cc,$cc,$ff,$cc,$ff,$ff
+	; Tile index,$075
+	dc.b $ff,$ff,$ff,$ff,$11,$ff,$11,$cc,$ff,$ff,$cc,$cc,$cc,$ff,$cc,$cc,$cc,$ff,$ff,$00,$00,$cc,$00,$cc,$00,$ff,$00,$00,$ff,$00,$00,$00
+	; Tile index,$076
+	dc.b $cc,$cc,$ff,$ff,$cc,$cc,$11,$ff,$cc,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$ff,$cc,$ff,$cc,$00,$cc,$cc,$ff
+	; Tile index,$077
+	dc.b $ff,$cc,$cc,$cc,$cc,$cc,$cc,$00,$ff,$11,$ff,$cc,$11,$cc,$cc,$ff,$cc,$cc,$00,$cc,$ff,$cc,$cc,$00,$ff,$ff,$ff,$11,$ff,$ff,$cc,$cc
+	; Tile index,$078
+	dc.b $cc,$cc,$00,$00,$00,$00,$00,$ff,$cc,$00,$00,$cc,$cc,$00,$00,$00,$11,$11,$00,$00,$00,$cc,$ff,$00,$ff,$00,$cc,$00,$00,$00,$00,$00
+	; Tile index,$079
+	dc.b $cc,$cc,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$cc,$cc,$cc,$00,$cc,$cc,$ff,$11,$11,$11,$ff,$cc,$cc,$ff,$ff,$11,$cc,$cc,$ff,$00,$11,$cc,$cc
+	; Tile index,$07A
+	dc.b $ff,$ff,$cc,$cc,$ff,$cc,$cc,$00,$cc,$cc,$cc,$cc,$cc,$ff,$ff,$cc,$ff,$ff,$ff,$cc,$ff,$ff,$ff,$00,$11,$ff,$cc,$00,$ff,$cc,$cc,$11
+	; Tile index,$07B
+	dc.b $ff,$00,$00,$00,$cc,$ff,$00,$00,$00,$cc,$00,$00,$00,$ff,$00,$11,$00,$cc,$cc,$00,$11,$ff,$cc,$ff,$cc,$ff,$11,$cc,$ff,$ff,$ff,$cc
+	; Tile index,$07C
+	dc.b $00,$ff,$cc,$ff,$00,$ff,$cc,$ff,$ff,$cc,$ff,$ff,$00,$ff,$cc,$ff,$cc,$cc,$ff,$cc,$ff,$cc,$ff,$ff,$11,$cc,$cc,$ff,$ff,$ff,$ff,$ff
+	; Tile index,$07D
+	dc.b $ff,$11,$ff,$11,$11,$cc,$ff,$cc,$11,$11,$ff,$11,$cc,$ff,$11,$ff,$ff,$cc,$cc,$cc,$11,$11,$ff,$cc,$11,$ff,$cc,$cc,$cc,$cc,$ff,$11
+	; Tile index,$07E
+	dc.b $ff,$11,$ff,$ff,$11,$cc,$00,$ff,$cc,$11,$00,$ff,$00,$cc,$11,$ff,$11,$00,$cc,$11,$cc,$00,$00,$cc,$ff,$cc,$ff,$ff,$ff,$11,$cc,$ff
+	; Tile index,$07F
+	dc.b $ff,$cc,$11,$ff,$cc,$ff,$ff,$ff,$11,$cc,$ff,$ff,$cc,$ff,$cc,$ff,$ff,$ff,$11,$ff,$11,$ff,$ff,$cc,$cc,$cc,$ff,$11,$ff,$11,$ff,$ff
